94b31a9df50e011bfb1d563e30df6c7ae5845f78
Cristopher Kurtin <cristopher.kurtin@xenon.hr>
2025-11-05 03:33:06 +0100

    chore: adapt the eslint config to allow `@ts-expect-error` directives without comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,24 @@
 import { generateEslintConfig } from '@companion-module/tools/eslint/config.mjs'
 
-export default generateEslintConfig({
+const baseConfig = generateEslintConfig({
 	enableTypescript: true,
 })
+
+const customConfig = [
+	...baseConfig,
+	{
+		rules: {
+			'@typescript-eslint/ban-ts-comment': [
+				'error',
+				{
+					'ts-expect-error': false,
+					'ts-ignore': true,
+					'ts-nocheck': true,
+					'ts-check': false,
+				},
+			],
+		},
+	},
+]
+
+export default customConfig
